2bdfb5c48ac9efda1167c6d2fcde77c1d32e1095
adan73 <167682529+adan73@users.noreply.github.com>
2025-04-16 18:28:23 +0300

    fixed the register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,11 @@ const register = async (req, res) => {
     });
 
     await newUser.save();
+    const token = jwt.sign(
+      { id: newUser._id, role: newUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
     res.status(201).json({ message: 'User created' });
   } catch (err) {
     res.status(500).json({ message: 'Error creating user', error: err });
